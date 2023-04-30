fe16ed74a32ded0e303391a2eb7fb454bd917762
victorbratov <victor@bratov.com>
2023-05-01 00:38:07 +0100

    Hibernate added

---
+++ src/main/java/com/example/op_sch/Main.java
@@ -0,0 +1,45 @@
+package com.example.op_sch;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args){
+        Doctor doc1 = new Doctor("Brown", "Eye doctor");
+        Doctor doc2 = new Doctor("Green", "Leg doctor");
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.persist(doc1);
+            session.persist(doc2);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List < Doctor > students = session.createQuery("from Doctor ", Doctor.class).list();
+            students.forEach(System.out::println);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+
+    }
+}
