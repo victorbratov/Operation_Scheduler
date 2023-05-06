a877a9918a184263d28d9088fd8db79185b25408
victorbratov <victor@bratov.com>
2023-05-06 11:23:35 +0100

    Screen manager for the gui

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,6 @@ module com.example.op_sch {
     opens com.example.op_sch to javafx.fxml, org.hibernate.orm.core, javafx.graphics;
     exports com.example.op_sch;
     exports com.example.op_sch.GUI.AllScreens;
-    exports com.example.op_sch.GUI.EntryPoint;
+    exports com.example.op_sch.GUI;
     opens com.example.op_sch.GUI.AllScreens to javafx.fxml, org.hibernate.orm.core;
 }
